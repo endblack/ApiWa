1689c8ecbf17acc73e7dfc3cbcba92fdad27e98d
Megah <75638685+endblack@users.noreply.github.com>
2023-12-01 23:08:10 -0300

    Add files via upload

---
+++ i2.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+[[ "$(whoami)" != "root" ]] && {
+    echo
+    echo "Instale com usuario root!"
+    echo
+    exit 0
+}
+[[ -e /root/apiWa/app.js ]] && {
+  echo
+  echo "Atualizando api...."
+  echo
+  curl https://raw.githubusercontent.com/endblack/ApiWa/main/apiWa.zip > apiWa.zip > /dev/null 2>&1
+  curl https://raw.githubusercontent.com/endblack/ApiWa/main/apioff > apioff > /dev/null 2>&1
+  curl https://raw.githubusercontent.com/endblack/ApiWa/main/apion > apion > /dev/null 2>&1
+  chmod 777 apion apioff
+  mv apion apioff /bin
+  rm -r /root/apiWa
+  unzip apiWa.zip
+  rm apiWa.zip
+  echo
+  echo "Atualização terminada! use apion para reiniciar"
+  echo
+  echo "Não esqueça de editar a porta no arquivo /root/apiWa/dados.js"
+  echo
+  exit 0
+}
+apt update -y
+echo
+echo "Instalado a api e suas dependências, aguarde..."
+echo
+apt install nodejs -y > /dev/null 2>&1
+apt install unzip -y > /dev/null 2>&1
+apt install screen -y > /dev/null 2>&1
+apt install wget -y > /dev/null 2>&1
+curl https://raw.githubusercontent.com/endblack/ApiWa/main/apiWa.zip > apiWa.zip > /dev/null 2>&1
+curl https://raw.githubusercontent.com/endblack/ApiWa/main/apioff > apioff > /dev/null 2>&1
+curl https://raw.githubusercontent.com/endblack/ApiWa/main/apion > apion > /dev/null 2>&1
+chmod 777 apion apioff
+mv apion apioff /bin
+unzip apiWa.zip
+rm apiWa.zip
+echo 
+echo "Instalação da Api terminada"
+echo
+echo "Adicione a porta no arquivo /root/apiWa/dados.js"
+echo
+echo "Depois use o comando apion para ativar"
