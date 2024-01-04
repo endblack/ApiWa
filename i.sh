1725c2b9ea9c9fef570539a8f5383e24f43e53f3
Megah <75638685+endblack@users.noreply.github.com>
2024-01-04 19:19:06 -0300

    Add files via upload

--- i.sh
+++ i.sh
@@ -10,18 +10,29 @@
   echo
   echo "Atualizando api...."
   echo
-  wget https://raw.githubusercontent.com/endblack/ApiWa/main/apiWa.zip > /dev/null 2>&1
+  wget https://raw.githubusercontent.com/endblack/ApiWa/main/src/user.sh > /dev/null 2>&1
+  wget https://raw.githubusercontent.com/endblack/ApiWa/main/src/app.js > /dev/null 2>&1
+  wget https://raw.githubusercontent.com/endblack/ApiWa/main/src/favicon.ico > /dev/null 2>&1
   wget https://raw.githubusercontent.com/endblack/ApiWa/main/apioff > /dev/null 2>&1
   wget https://raw.githubusercontent.com/endblack/ApiWa/main/apion > /dev/null 2>&1
   chmod 777 apion apioff
   mv apion apioff /bin
-  rm -r /root/apiWa
-  unzip apiWa.zip
-  rm apiWa.zip
+  rm /root/apiWa/src/user.sh /root/apiWa/app.js
+  mv user.sh /root/apiWa/src
+  mv app.js favicon.ico /root/apiWa
   echo
   echo "Atualização terminada! use apion para reiniciar"
   echo
-  echo "Não esqueça de editar a porta no arquivo /root/apiWa/dados.js"
+  #echo "Não esqueça de editar a porta no arquivo /root/apiWa/dados.js"
+  echo
+  [[ -e /root/apiWa/token.txt ]] && {
+  exit 0
+  }
+  echo "Token de acesso a sua api, guarde com SEGURANÇA:"
+  echo
+  token=$(node -e "console.log(require('crypto').randomBytes(50).toString('base64'));")
+  echo $token
+  echo $token > /root/apiWa/token.txt
   echo
   exit 0
 }
@@ -40,9 +51,16 @@ chmod 777 apion apioff
 mv apion apioff /bin
 unzip apiWa.zip
 rm apiWa.zip
-echo 
 echo "Instalação da Api terminada"
 echo
 echo "Adicione a porta no arquivo /root/apiWa/dados.js"
 echo
 echo "Depois use o comando apion para ativar"
+echo
+echo "Token de acesso a sua api, guarde com SEGURANÇA:"
+echo
+token=$(node -e "console.log(require('crypto').randomBytes(50).toString('base64'));")
+echo $token
+echo $token > /root/apiWa/token.txt
+echo 
+cd
--- i2.sh
+++ i2.sh
@@ -10,19 +10,31 @@
   echo
   echo "Atualizando api...."
   echo
-  curl https://raw.githubusercontent.com/endblack/ApiWa/main/apiWa.zip > apiWa.zip
+  curl https://raw.githubusercontent.com/endblack/ApiWa/main/src/favicon.ico > favicon.ico
+  curl https://raw.githubusercontent.com/endblack/ApiWa/main/src/user.sh > user.sh
+  curl https://raw.githubusercontent.com/endblack/ApiWa/main/src/app.js > app.sh
   curl https://raw.githubusercontent.com/endblack/ApiWa/main/apioff > apioff
   curl https://raw.githubusercontent.com/endblack/ApiWa/main/apion > apion
   chmod 777 apion apioff
   mv apion apioff /bin
-  rm -r /root/apiWa
-  unzip apiWa.zip
-  rm apiWa.zip
+  rm /root/apiWa/src/user.sh /root/apiWa/app.js
+  mv user.sh /root/apiWa/src
+  mv app.js favicon.ico /root/apiWa
   echo
   echo "Atualização terminada! use apion para reiniciar"
   echo
-  echo "Não esqueça de editar a porta no arquivo /root/apiWa/dados.js"
+  #echo "Não esqueça de editar a porta no arquivo /root/apiWa/dados.js"
   echo
+  [[ -e /root/apiWa/token.txt ]] && {
+  exit 0
+  }
+  echo "Token de acesso a sua api, guarde com SEGURANÇA:"
+  echo
+  token=$(node -e "console.log(require('crypto').randomBytes(50).toString('base64'));")
+  echo $token
+  echo $token > /root/apiWa/token.txt
+  echo
+  cd
   exit 0
 }
 apt update -y
@@ -46,3 +58,8 @@ echo
 echo "Adicione a porta no arquivo /root/apiWa/dados.js"
 echo
 echo "Depois use o comando apion para ativar"
+echo "Token de acesso a sua api, guarde com SEGURANÇA:"
+echo
+echo $token
+echo $token > /root/apiWa/token.txt
+echo 
